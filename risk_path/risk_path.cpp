f42a280a0458f636493925ce7975bc2770ffb766
cramke <carsten.ramke@gmail.com>
2021-03-30 11:47:12 +0200

    real space without error

--- risk_path/risk_path.cpp
+++ risk_path/risk_path.cpp
@@ -1,5 +1,41 @@
+/*********************************************************************
+ * Software License Agreement (BSD License)
+ *
+ *  Copyright (c) 2010, Rice University
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above
+ *     copyright notice, this list of conditions and the following
+ *     disclaimer in the documentation and/or other materials provided
+ *     with the distribution.
+ *   * Neither the name of the Rice University nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *  POSSIBILITY OF SUCH DAMAGE.
+ *********************************************************************/
+
+ /* Author: Ioan Sucan */
+
 #include <ompl/base/SpaceInformation.h>
-#include <ompl/base/spaces/SE2StateSpace.h>
+#include <ompl/base/spaces/SE3StateSpace.h>
 #include <ompl/base/spaces/RealVectorStateSpace.h>
 #include <ompl/geometric/planners/rrt/RRTConnect.h>
 #include <ompl/geometric/SimpleSetup.h>
@@ -12,34 +48,92 @@ namespace og = ompl::geometric;
 
 bool isStateValid(const ob::State* state)
 {
+    // extract the first component of the state and cast it to what we expect
     const ob::RealVectorStateSpace::StateType* state2D = state->as<ob::RealVectorStateSpace::StateType>();
 
     // Extract the robot's (x,y) position from its state
     double x = state2D->values[0];
     double y = state2D->values[1];
 
-    if (x > -0.5 && x < 0.5 && y > -0.5 && y < 0.5)
+    // return a value that is always true but uses the two variables we define, so we avoid compiler warnings
+    return x != y;
+}
+
+void plan()
+{
+    // construct the state space we are planning in
+    auto space(std::make_shared<ob::RealVectorStateSpace>(2));
+
+    // set the bounds for the R^3 part of SE(3)
+    ob::RealVectorBounds bounds(2);
+    bounds.setLow(-1);
+    bounds.setHigh(1);
+
+    space->setBounds(bounds);
+
+    // construct an instance of  space information from this state space
+    auto si(std::make_shared<ob::SpaceInformation>(space));
+
+    // set state validity checking for this space
+    si->setStateValidityChecker(isStateValid);
+
+    // create a random start state
+    ob::ScopedState<> start(space);
+    start.random();
+
+    // create a random goal state
+    ob::ScopedState<> goal(space);
+    goal.random();
+
+    // create a problem instance
+    auto pdef(std::make_shared<ob::ProblemDefinition>(si));
+
+    // set the start and goal states
+    pdef->setStartAndGoalStates(start, goal);
+
+    // create a planner for the defined space
+    auto planner(std::make_shared<og::RRTConnect>(si));
+
+    // set the problem we are trying to solve for the planner
+    planner->setProblemDefinition(pdef);
+
+    // perform setup steps for the planner
+    planner->setup();
+
+
+    // print the settings for this space
+    si->printSettings(std::cout);
+
+    // print the problem settings
+    pdef->print(std::cout);
+
+    // attempt to solve the problem within one second of planning time
+    ob::PlannerStatus solved = planner->ob::Planner::solve(1.0);
+
+    if (solved)
     {
-        
-        return false;
+        // get the goal representation from the problem definition (not the same as the goal state)
+        // and inquire about the found path
+        ob::PathPtr path = pdef->getSolutionPath();
+        std::cout << "Found solution:" << std::endl;
+
+        // print the path to screen
+        path->print(std::cout);
     }
     else
-    {
-        std::cout << x << std::endl;
-        std::cout << y << std::endl;
-        std::cout << std::endl;
-        return true;
-    }
+        std::cout << "No solution found" << std::endl;
 }
 
 void planWithSimpleSetup()
 {
     // construct the state space we are planning in
-    auto space(std::make_shared<ob::RealVectorStateSpace>(2));
+    auto space(std::make_shared<ob::SE3StateSpace>());
 
-    ob::RealVectorBounds bounds(2);
+    // set the bounds for the R^3 part of SE(3)
+    ob::RealVectorBounds bounds(3);
     bounds.setLow(-1);
     bounds.setHigh(1);
+
     space->setBounds(bounds);
 
     // define a simple setup class
@@ -48,23 +142,23 @@ void planWithSimpleSetup()
     // set state validity checking for this space
     ss.setStateValidityChecker([](const ob::State* state) { return isStateValid(state); });
 
-    // set the start and goal states
+    // create a random start state
     ob::ScopedState<> start(space);
-    start->as<ob::RealVectorStateSpace::StateType>()->values[0] = -1.0;
-    start->as<ob::RealVectorStateSpace::StateType>()->values[1] = -1.0;
+    start.random();
 
+    // create a random goal state
     ob::ScopedState<> goal(space);
-    goal->as<ob::RealVectorStateSpace::StateType>()->values[0] = 1.0;
-    goal->as<ob::RealVectorStateSpace::StateType>()->values[1] = 1.0;
+    goal.random();
 
+    // set the start and goal states
     ss.setStartAndGoalStates(start, goal);
 
-    // to get more output information
+    // this call is optional, but we put it in to get more output information
     ss.setup();
     ss.print();
 
     // attempt to solve the problem within one second of planning time
-    ob::PlannerStatus solved = ss.solve(3);
+    ob::PlannerStatus solved = ss.solve(1.0);
 
     if (solved)
     {
@@ -80,6 +174,8 @@ void planWithSimpleSetup()
 int main(int /*argc*/, char** /*argv*/)
 {
     std::cout << "OMPL version: " << OMPL_VERSION << std::endl;
+
+    plan();
 
     std::cout << std::endl << std::endl;
 
