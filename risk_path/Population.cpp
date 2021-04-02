257afbe4127912ed491a3d80a9e9398abba8c1cf
cramke <carsten.ramke@gmail.com>
2021-04-02 20:32:12 +0200

    population map class

--- risk_path/Population.cpp
+++ risk_path/Population.cpp
@@ -2,75 +2,86 @@
 #include "cpl_conv.h" // for CPLMalloc()
 #include <iostream>
 
-void IndexAsSpatial(int x, int y)
-{
-    // if getGeoTransform is:
-    // 5.86764    0.000277778    0
-    //55.0568    0 - 0.000277778
-    double xd = 5.86764 + 0.000277778 * x + 0 * y;
-    double yd = 55.0568 + 0 * x + (-1) * 0.000277778 * y;
-    std::cout << "X': " << xd << " Y': " << yd << std::endl;
-}
 
-void SpatialAsIndex(double xd, double yd)
-{
-    // if getGeoTransform is:
-    // 5.86764    0.000277778    0
-    //55.0568    0 - 0.000277778
-    double x = (xd - 5.86764 - yd * 0) / 0.000277778;
-    double y = (yd - 55.0568 - xd * 0) / 0.000277778 * (-1);
-    x = std::round(x);
-    y = std::round(y);
-    int x_index = int(x);
-    int y_index = int(y);
-    std::cout << "X: " << x_index << " Y: " << y_index << std::endl;
-}
-
-int main()
-{
-    GDALDataset* poDataset;
-    GDALAllRegister();
-    poDataset = (GDALDataset*)GDALOpen("maps/pop_deu.tif", GA_ReadOnly);
-    int rasterCount = poDataset->GetRasterCount();
-    if (poDataset != NULL)
-    {
-        GDALRasterBand* poBand;
-        int             nBlockXSize, nBlockYSize;
-        
-        poBand = poDataset->GetRasterBand(1);
-        poBand->GetBlockSize(&nBlockXSize, &nBlockYSize);
-        printf("Block=%dx%d Type=%s, ColorInterp=%s\n",
-            nBlockXSize, nBlockYSize,
-            GDALGetDataTypeName(poBand->GetRasterDataType()),
-            GDALGetColorInterpretationName(
-                poBand->GetColorInterpretation()));
-
-        float* pafScanline;
-        int   nXSize = poBand->GetXSize();
-        std::cout << "XSize: " << nXSize << std::endl;
-        int   nYSize = poBand->GetYSize();
-        std::cout << "YSize: " << nYSize << std::endl;
-
-        pafScanline = (float*)CPLMalloc(sizeof(float) * 1);
-        CPLErr success;
-        success = poBand->RasterIO(GF_Read, 27046, 9315, 1, 1,
-            pafScanline, 1, 1, GDT_Float32,
-            0, 0);
-
-
-        std::cout << "People in raster: " << pafScanline[0] << std::endl;
-
+class PopMap {
+    private:
+        const char *filename = "maps/pop_deu.tif";
+        GDALDataset* dataset;
+        GDALRasterBand* band;
+        float* scanline = (float*)CPLMalloc(sizeof(float) * 1);
+        int             nXSize, nYSize;
+    public:
         double transform[6];
-        poDataset->GetGeoTransform(transform);
-        std::cout << transform[0] << "    " << transform[1] << "    " << transform[2] << std::endl;
-        std::cout << transform[3] << "    " << transform[4] << "    " << transform[5] << std::endl;
 
-        SpatialAsIndex(13.380331649, 52.469187773);
+        PopMap() {
+            GDALAllRegister();
+            dataset = (GDALDataset*)GDALOpen(filename, GA_ReadOnly);
+            if (dataset != NULL) 
+            {
+                std::cout << "Dataset successfully loaded." << std::endl;
+                dataset->GetGeoTransform(transform);
+                band = dataset->GetRasterBand(1);
+            }
+            else
+            {
+                printf("The dataset failed to open. Maybe check the filename?");
+            }
+        }
 
-        GDALClose(poDataset);
-    }
-    else
-    {
-        printf("The dataset failed to open. Maybe check the filename?");
-    }
+        
+        std::tuple<int, int> SpatialAsIndex(double xd, double yd)
+        {
+            // https://gdal.org/tutorials/geotransforms_tut.html
+            double x = (xd - transform[0] - yd * transform[2]) / transform[1];
+            double y = (yd - transform[3] - xd * transform[4]) / transform[5];
+            int x_index = int(std::round(x));
+            int y_index = int(std::round(y));
+            return std::make_tuple(x_index, y_index);
+        }
+
+        std::tuple<int, int> IndexAsSpatial(int x, int y)
+        {
+            double x_spatial = transform[0] + transform[1] * x + transform[2] * y;
+            double y_spatial = transform[3] + transform[4] * x + transform[5] * y;
+            std::cout << "X': " << x_spatial << " Y': " << y_spatial << std::endl;
+            return std::make_tuple(x_spatial, y_spatial);
+        }
+
+        float getPopIndex(int x, int y)
+        {
+            // 27046 9315
+            band->RasterIO(GF_Read, x, y, 1, 1, scanline, 1, 1, GDT_Float32, 0, 0);
+            return scanline[0];
+        }
+
+        float getPopGeo(double x_geo, double y_geo)
+        {
+            auto [x_index, y_index] = SpatialAsIndex(x_geo, y_geo);
+            return getPopIndex(x_index, y_index); 
+        }
+
+        void close()
+        {
+            GDALClose(dataset);
+        }
+
+        void printBandSize()
+        {
+            nXSize = band->GetXSize();
+            std::cout << "XSize: " << nXSize << std::endl;
+            nYSize = band->GetYSize();
+            std::cout << "YSize: " << nYSize << std::endl;
+        }
+
+};
+
+int test()
+{
+    PopMap ds = PopMap();
+    double pop1 = ds.getPopIndex(27046, 9315);
+    ds.IndexAsSpatial(27046, 9315);
+    std::cout << "Population: " << pop1 << std::endl;
+    double pop2 = ds.getPopGeo(13.3804, 52.4693);
+    std::cout << "Population: " << pop2 << std::endl;
 }
+
