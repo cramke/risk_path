3405fdfdb2444b7fa647fe6436c4e27ee5129774
cramke <carsten.ramke@gmail.com>
2021-08-02 22:25:56 +0200

    Changed space to RealVectorSpace(3) Set realistic start & goal

--- risk_path/risk_path.cpp
+++ risk_path/risk_path.cpp
@@ -6,37 +6,33 @@
 #include <ompl/config.h>
 #include <iostream>
 #include "Population.cpp"
+#include "risk_path.h"
 
 namespace ob = ompl::base;
 namespace og = ompl::geometric;
 
 bool isStateValid(const ob::State* state)
 {
-    // cast the abstract state type to the type we expect
-    const auto* se3state = state->as<ob::SE3StateSpace::StateType>();
+    const double* pos = state->as<ob::RealVectorStateSpace::StateType>()->values;
 
-    // extract the first component of the state and cast it to what we expect
-    const auto* pos = se3state->as<ob::RealVectorStateSpace::StateType>(0);
-    // const auto* rot = se3state->as<ob::SO3StateSpace::StateType>(1);
-    const ob::SO3StateSpace::StateType& rot = se3state->rotation();
-
-    Coordinates* point = new Coordinates(se3state->getX(), se3state->getY());
+    PopulationMap map = PopulationMap();
+    Coordinates* point = new Coordinates(12, 8, map.transform);
 
     // return a value that is always true but uses the two variables we define, so we avoid compiler warnings
-    return se3state->getX() != se3state->getY();
+    return pos[2] == 100 && pos[1] != pos[0];
 }
 
 
 void planWithSimpleSetup()
 {
-    auto space(std::make_shared<ob::SE3StateSpace>());
+    auto space(std::make_shared<ob::RealVectorStateSpace>(3));
 
     ob::RealVectorBounds bounds(3);
-    bounds.setLow(0,-1);
-    bounds.setHigh(0,1);
-    bounds.setLow(1,-1);
-    bounds.setHigh(1,1);
-    bounds.setLow(2,0);
+    bounds.setLow(0,47);
+    bounds.setHigh(0,55);
+    bounds.setLow(1,5.86);
+    bounds.setHigh(1,15);
+    bounds.setLow(2,100);
     bounds.setHigh(2,100);
     space->setBounds(bounds);
 
@@ -48,23 +44,25 @@ void planWithSimpleSetup()
 
     // create a random start / goal state
     ob::ScopedState<> start(space);
-    start.random();
+    start[0] = 49.86462268679067;
+    start[1] = 8.657507656252882;
+    start[2] = 100;
     ob::ScopedState<> goal(space);
-    goal.random();
+    goal[0] = 50.107998827159896;
+    goal[1] = 8.68757388575945;
+    goal[2] = 100;
     ss.setStartAndGoalStates(start, goal);
 
     // this call is optional, but we put it in to get more output information
     ss.setup();
     ss.print();
 
-    const int PLANNING_TIME = 0.1;
+    const int PLANNING_TIME = 1;
     ob::PlannerStatus solved = ss.solve(PLANNING_TIME);
 
     if (solved)
     {
         std::cout << "Found solution:" << std::endl;
-        // print the path to screen
-        ss.simplifySolution();
         ss.getSolutionPath().print(std::cout);
     }
     else
