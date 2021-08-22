cb5f2ee2bc21d4f5b3f96aa2565818eb8822fa96
cramke <carsten.ramke@gmail.com>
2021-08-22 18:36:10 +0200

    Moved main function into its own file

---
+++ risk_path/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include "risk_path.cpp"
+
+void plan_env_1()
+{
+    std::array<double, 3> start_point = { 49.86462268679067, 8.657507656252882, 100 };
+    std::array<double, 3> goal_point = { 50.107998827159896, 8.68757388575945, 100 };
+    auto pop_map = std::make_shared<PopulationMap>();
+
+    PlanningSetup planner = PlanningSetup();
+    planner.set_start_goal(start_point, goal_point);
+    planner.set_boundaries();
+    planner.set_validity_checker(start_point, goal_point);
+    planner.set_objective(pop_map);
+    planner.solve();
+}
+
+int main(int /*argc*/, char** /*argv*/)
+{
+    plan_env_1();
+}
