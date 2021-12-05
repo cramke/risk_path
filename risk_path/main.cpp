1b1674ec5610854b0883745ce8925b0fed60b4a5
cramke <carsten.ramke@gmail.com>
2021-12-05 10:32:34 +0100

    code formatting

--- risk_path/main.cpp
+++ risk_path/main.cpp
@@ -2,13 +2,6 @@
 #include <iostream>
 #include "risk_path.h"
 
-void load_points()
-{
-    const char* filename = "C:/Users/carst/OneDrive/Projekte/risk-path/risk_path/data/ghs_pop_random_sample_points.geojson";
-    GeoJsonReader reader = GeoJsonReader(filename);
-    auto points = reader.get_points();
-    std::shared_ptr<RTree> rtree = std::make_shared<RTree>(points);
-}
 
 void plan_env_pop_rtree()
 {
@@ -24,24 +17,28 @@ void plan_env_pop_rtree()
     auto points = reader.get_points();
     std::shared_ptr<RTree> rtree = std::make_shared<RTree>(points);
     planner.set_rtee_objective(rtree);
+
+    const char* path = "C:/Users/carst/OneDrive/Projekte/risk-path/risk_path/data/test.geojson";
+    planner.set_validity_checker(path);
     
     planner.set_boundaries();
-    
     planner.solve();
 }
 
 void plan_env_1()
 {
+    PlanningSetup planner = PlanningSetup();
     std::array<double, 3> start_point = { 49.86462268679067, 8.657507656252882, 100 };
     std::array<double, 3> goal_point = { 50.107998827159896, 8.68757388575945, 100 };
-    auto pop_map = std::make_shared<PopulationMap>();
-    std::string path = "C:/Users/carst/OneDrive/Projekte/risk-path/risk_path/data/test.geojson";
-
-    PlanningSetup planner = PlanningSetup();
     planner.set_start_goal(start_point, goal_point);
-    planner.set_boundaries();
-    planner.set_validity_checker(path);
+    
+    auto pop_map = std::make_shared<PopulationMap>();
     planner.set_objective(pop_map);
+
+    const char* path = "C:/Users/carst/OneDrive/Projekte/risk-path/risk_path/data/test.geojson";
+    planner.set_validity_checker(path);
+
+    planner.set_boundaries();
     planner.solve();
 }
 
